3041022ad6f5d8745bdd42dcafd1fc9d5d0a02b0
Syafiq Fajrian Emha <syafiqfajrianemha11@gmail.com>
2020-06-09 12:19:03 +0700

    migration dan membuat tabel products

---
+++ database/migrations/2020_06_09_050709_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('description');
+            $table->bigInteger('price');
+            $table->string('category', 100);
+            $table->string('image', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
